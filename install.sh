edbf98f235a5efdba9f172d418795d16aa85f847
Rama-X2 <aderamadhaniputra35@gmail.com>
2026-02-17 18:13:49 +0700

    add install.sh and fix bug

---
+++ install.sh
@@ -0,0 +1,43 @@
+# Copyright (C) 2026 Rama-X2
+# Licensed under the GNU General Public License, Version 3.0
+# Source: https://github.com/Rama-X2/gms-breaker-miyabi-core
+
+#!/system/bin/sh
+
+print_modname() {
+  ui_print "======================================================"
+busybox sleep 1
+  ui_print "                   "
+busybox sleep 1
+  ui_print " Created by : Rama-X2"
+busybox sleep 1
+  ui_print " Install BusyBox"
+  ui_print "               "
+  ui_print " github : https://github.com/Rama-X2 "
+busybox sleep 1
+  ui_print "⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀"
+busybox sleep 1
+  ui_print "           ╔══════════════════════╗               "
+  ui_print "           ║           (≧◡≦)          ║              "
+  ui_print "           ║        Miyabi Core       ║              "
+  ui_print "           ╚══════════════════════╝               "
+busybox sleep 1  
+  ui_print "                      ／l、                          "
+  ui_print "                    （ﾟ､ ｡ ７                        "
+  ui_print "                     l、  ~ヽ                        "
+  ui_print "                     じしf_, )ノ                     "
+  busybox sleep 1
+  ui_print "     『 GMS Breaker Miyabi Core v1.0.1 』          "
+  busybox sleep 1
+  ui_print "⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀"
+  busybox sleep 1
+  ui_print " - Initializing Runtime Engine..."
+  busybox sleep 1
+  ui_print " - Applying GMS Isolation..."
+  ui_print " - Finalizing.."
+  busybox sleep 1
+  ui_print " - Done."
+  busybox sleep 1
+  ui_print "==================『 REBOOT DEVICE 』=================="
+  ui_print "                   "
+}
